5988095f53c083bea7c454a98bdfc9797528cd08
Allan Roberto Da Silva Gomes <allanrs@ps.grupopanvel.com.br>
2025-09-01 22:17:36 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ServicesComponent,
+    AboutComponent,
+    TestimonialsComponent,
+    PackagesComponent,
+    ContactComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
